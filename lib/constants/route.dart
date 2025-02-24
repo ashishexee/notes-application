9ed5e5dc2b607e7f96f6bef4275f7794e3b41937
ashish <144021866+ashishexee@users.noreply.github.com>
2025-02-24 18:35:43 +0530

    added a new route

--- lib/constants/route.dart
+++ lib/constants/route.dart
@@ -2,3 +2,4 @@ const loginroute = '/login/';
 const registerroute = '/register/';
 const notesroute = '/notes';
 const emailverifyroute = '/verify-email/';
+const newnoteroute = '/notes/new-note/'; // created a new route
