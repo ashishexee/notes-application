7241da3f5f3f8c5993fed3b60508ab4de0b6710b
ashish <144021866+ashishexee@users.noreply.github.com>
2025-03-19 00:39:17 +0530

    delete note functionality

--- lib/views/note/notes_view.dart
+++ lib/views/note/notes_view.dart
@@ -22,6 +22,91 @@ class _NotesViewState extends State<NotesView> {
     super.initState();
   }
 
+  void _deletenote(context, int noteid) async {
+    await showDialog(
+      context: context,
+      builder: (context) {
+        final AnimationController controller = AnimationController(
+          duration: const Duration(milliseconds: 500),
+          vsync: Navigator.of(context),
+        );
+        final Animation<double> scaleAnimation = CurvedAnimation(
+          parent: controller,
+          curve: Curves.easeInOut,
+        );
+
+        controller.forward();
+
+        return ScaleTransition(
+          scale: scaleAnimation,
+          child: AlertDialog(
+            title: Row(
+              children: const [
+                Icon(Icons.delete, color: Colors.blue),
+                SizedBox(width: 8),
+                Text(
+                  'Delete ',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                  ),
+                ),
+                Text(
+                  'Note',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 5, 96, 171),
+                  ),
+                ),
+              ],
+            ),
+            content: const Text(
+              'Are you sure you want to delete this note?',
+              style: TextStyle(fontSize: 16),
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  controller.reverse().then((_) {
+                    Navigator.of(context).pop(false);
+                  });
+                },
+                child: Row(
+                  children: const [
+                    Icon(Icons.cancel, color: Colors.red),
+                    SizedBox(width: 4),
+                    Text(
+                      'Cancel',
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  ],
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  _notesServices.deletenote(id: noteid);
+                  controller.reverse().then((_) {
+                    Navigator.of(context).pop(true);
+                  });
+                },
+                child: Row(
+                  children: const [
+                    Icon(Icons.delete_forever, color: Colors.blue),
+                    SizedBox(width: 4),
+                    Text(
+                      'Delete',
+                      style: TextStyle(color: Colors.blue),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -83,20 +168,69 @@ class _NotesViewState extends State<NotesView> {
                         final allnotes = snapshot.data as List<DatabaseNote>;
                         devtools.log('Fetched ${allnotes.length} notes');
                         return ListView.builder(
+                          padding: const EdgeInsets.all(16.0),
                           itemCount: allnotes.length,
                           itemBuilder: (context, index) {
                             final note = allnotes[index];
-                            return ListTile(
-                              title: Text(
-                                note.text,
-                                maxLines: 1,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                              onTap: () {
-                                // Navigate to edit note view
-                                Navigator.of(context).pushNamed(
-                                  newnoteroute,
-                                  arguments: note,
+                            return StatefulBuilder(
+                              builder: (context, setState) {
+                                bool isHovered = false;
+                                return MouseRegion(
+                                  onEnter: (_) {
+                                    setState(() {
+                                      isHovered = true;
+                                    });
+                                  },
+                                  onExit: (_) {
+                                    setState(() {
+                                      isHovered = false;
+                                    });
+                                  },
+                                  child: AnimatedContainer(
+                                    duration: const Duration(milliseconds: 300),
+                                    transform: isHovered
+                                        ? (Matrix4.identity()
+                                          ..translate(0, -10, 0))
+                                        : Matrix4.identity(),
+                                    child: Card(
+                                      margin: const EdgeInsets.symmetric(
+                                          vertical: 8),
+                                      elevation: 4,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                      child: ListTile(
+                                        contentPadding:
+                                            const EdgeInsets.symmetric(
+                                                vertical: 8, horizontal: 16),
+                                        title: Text(
+                                          note.text,
+                                          maxLines: 1,
+                                          overflow: TextOverflow.ellipsis,
+                                          style: const TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w500,
+                                          ),
+                                        ),
+                                        trailing: IconButton(
+                                          onPressed: () {
+                                            _deletenote(context, note.id);
+                                            setState(() {});
+                                          },
+                                          icon: const Icon(
+                                            Icons.delete,
+                                            color: Colors.blue,
+                                          ),
+                                        ),
+                                        onTap: () {
+                                          Navigator.of(context).pushNamed(
+                                            newnoteroute,
+                                            arguments: note,
+                                          );
+                                        },
+                                      ),
+                                    ),
+                                  ),
                                 );
                               },
                             );
@@ -104,16 +238,26 @@ class _NotesViewState extends State<NotesView> {
                         );
                       } else {
                         return const Center(
-                          child: Text('No Notes Yet'),
+                          child: Text(
+                            'No Notes Yet',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
                         );
                       }
                     default:
-                      return const CircularProgressIndicator();
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
                   }
                 },
               );
             default:
-              return const CircularProgressIndicator();
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
           }
         },
       ),
